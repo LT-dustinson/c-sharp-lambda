6af70ab53db1dc6d650319ca355e2ee1a41b1e5c
Josh Angolano <jangolano@leantechniques.com>
2022-06-03 10:36:57 -0500

    Added simple resource

--- tf/api-gateway.tf
+++ tf/api-gateway.tf
@@ -4,3 +4,12 @@ resource "aws_api_gateway_rest_api" "csharp-api" {
   description        = "Sample CSharp API"
   binary_media_types = ["image/png"]
 }
+
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "hello"
+  parent_id   = aws_api_gateway_rest_api.csharp-api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.csharp-api.id
+}
+
+
