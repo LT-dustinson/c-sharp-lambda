a7ad15cef26b16f2ba479f4b6d609126922ca863
Josh Angolano <jangolano@leantechniques.com>
2022-06-03 09:35:37 -0500

    Adding lambda function to terraform

---
+++ tf/lambda.tf
@@ -0,0 +1,11 @@
+
+resource "aws_lambda_function" "c_sharp_lambda" {
+  filename      = "BlogFunction.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "BlogFunction::BlogFunction.Function::FunctionHandler"
+  source_code_hash = filebase64sha256("BlogFunction.zip")
+
+  runtime = "nodejs12.x"
+  
+}
