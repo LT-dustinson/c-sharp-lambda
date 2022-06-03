96a4adadb142f493265ee2dd8e014a0f93bac57d
Josh Angolano <jangolano@leantechniques.com>
2022-06-03 10:56:10 -0500

    Invoke Lambda

--- tf/api-gateway.tf
+++ tf/api-gateway.tf
@@ -13,9 +13,19 @@ resource "aws_api_gateway_resource" "resource" {
 }
 
 
-resource "aws_api_gateway_method" "MyDemoMethod" {
+resource "aws_api_gateway_method" "csharp-api" {
   rest_api_id   = aws_api_gateway_rest_api.csharp-api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "GET"
   authorization = "NONE"
+}
+
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.csharp-api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.csharp-api.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.c_sharp_lambda.invoke_arn
 }
