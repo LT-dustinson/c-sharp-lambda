2f4d01a8a461244cd189a81e27de899eceae85ba
Josh Angolano <jangolano@leantechniques.com>
2022-06-03 14:37:01 -0500

    Added the stage

--- tf/api-gateway.tf
+++ tf/api-gateway.tf
@@ -52,3 +52,12 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
   source_arn = "arn:aws:execute-api:${var.region}:${var.aws_account}:${aws_api_gateway_rest_api.csharp-api.id}/*/${aws_api_gateway_method.csharp-api.http_method}${aws_api_gateway_resource.resource.path}"
 }
+
+###########################################################
+# Add a stage so the the api is accessible
+###########################################################
+resource "aws_api_gateway_deployment" "test_gateway_deployment" {
+  depends_on  = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.csharp-api.id
+  stage_name  = "prod"
+}
