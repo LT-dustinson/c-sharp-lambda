e385d15794f2ae63425430fcc88d413ffd2f95eb
Josh Angolano <jangolano@leantechniques.com>
2022-06-03 09:01:12 -0500

    Added Terraform Init Steps

---
+++ tf/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+backend "s3" {
+  bucket = "c-sharp-terraform-state"
+  key    = "state"
+  region = "us-east-1"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
