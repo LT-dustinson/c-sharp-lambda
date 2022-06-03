470d7c5d8d6dcd15ab98e8509ef55ad91fae16d0
Josh Angolano <jangolano@leantechniques.com>
2022-06-03 09:09:31 -0500

    Moved the terraform backend configuration

--- tf/provider.tf
+++ tf/provider.tf
@@ -1,17 +1,17 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+  backend "s3" {
+    bucket = "c-sharp-terraform-state"
+    key    = "state"
+    region = "us-east-1"
+  }
+  aws = {
+    required_providers {
+    source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
 }
 
-backend "s3" {
-  bucket = "c-sharp-terraform-state"
-  key    = "state"
-  region = "us-east-1"
-}
 
 # Configure the AWS Provider
 provider "aws" {
