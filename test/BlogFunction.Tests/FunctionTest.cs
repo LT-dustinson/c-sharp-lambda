dce2e7ac2d46a3ecc609aea2cfc824055b219631
Dustin Thostenson <sourcetree@delta3consulting.com>
2022-06-03 22:08:50 -0500

    test new message displays in aws (previous test fail failed_

--- test/BlogFunction.Tests/FunctionTest.cs
+++ test/BlogFunction.Tests/FunctionTest.cs
@@ -9,13 +9,12 @@ public class FunctionTest
     [Fact]
     public void TestToUpperFunction()
     {
-
         // Invoke the lambda function and confirm the string was upper cased.
         var function = new Function();
         var context = new TestLambdaContext();
         var request = new APIGatewayProxyRequest();
         var response = function.FunctionHandler(request, context);
 
-        Assert.Equal("Hello World", response.Body);
+        Assert.StartsWith("Hello World", response.Body);
     }
 }
